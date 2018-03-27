d6ec5f5d7955de6f1af919f794c21ded7da29664
Ray Snider <ray.snider21@gmail.com>
2018-03-27 17:14:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ function deleteFromObjectByKey(obj, key)
   delete newObj[key];
   return newObj;
 }
+
+function destructivelyDeleteFromObjectByKey(obj, key)
+{
+  delete obj[key];
+  return obj;
+}
